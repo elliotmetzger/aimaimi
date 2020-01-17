cc963283463a271ddf1a0e74ce0d6721ac26ab85
elliotmetzger <60015704+elliotmetzger@users.noreply.github.com>
2020-01-17 12:33:59 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         'HYiUgO6JLcYVdphCcnwelV69S',
+  consumer_secret:      'iU1Lyuy3q0G4UKdsGiwWBH6sxm91t4NPBmQbEjPrYwqYlAyADt',
+  access_token:         '1218044669663166464-UXidRyjdvDuyVlifhagtTaZW07yQvs',
+  access_token_secret:  'FoUZKekoAwMR01NBQNbhV4JImqPKCR9IcXpLzzY5X75jV'
+}
+
+module.exports = config;
